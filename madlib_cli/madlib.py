9ac71cc8152fa49326b4cee5f8c1a947ab83da57
Nadeemmazahreh <nadeemmazahreh97@hotmail.com>
2021-07-28 16:38:44 +0300

    error adjusted

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,7 +1,5 @@
 import re
 
-print("""Hello, you should enter a series of words depending on the type of word (can be adgective, verb, noun etc..), and the algroithm will place those words in a text and will show the super funny output on the screen\n""")
-
 def read_template(filename: str):
     try:
         with open(filename, 'r') as file:
@@ -10,9 +8,6 @@ def read_template(filename: str):
     except:
         FileNotFoundError
 
-madlib_text = read_template('assets/madlib.txt')
-
-print(madlib_text + "\n")
 
 def parse_template(string):
     words = re.findall(r'{.+?}', string)
@@ -22,17 +17,30 @@ def parse_template(string):
     new_string = re.sub(r'{.+?}',r'{}', string)
     return new_string, all_words
 
-parsed_string, all_words = parse_template(madlib_text)
-
-list_word_input = []
-for i in range(0,len(all_words)):
-    word = input('Please write the words you want to fill ')
-    list_word_input.append(word)
 
 def merge(string,list):
     new_string = string.format(*list)
-    print(new_string)
     return new_string
 
-print(merge(parsed_string,list_word_input))
+
+def main():
+
+    print("""Hello, you should enter a series of words depending on the type of word (can be adgective, verb, noun etc..), and the algroithm will place those words in a text and will show the super funny output on the screen\n""")
+
+    madlib_text = read_template('assets/test.txt')
+
+    parsed_string, all_words = parse_template(madlib_text)
+
+    print("Please Provide the following")
+    print(all_words)
+
+    list_word_input = []
+    for i in range(0,len(all_words)):
+        word = input('Please write the words you want to fill ')
+        list_word_input.append(word)
+
+    print(merge(parsed_string,list_word_input))
+
+if __name__ == "__main__":
+    main()
 
